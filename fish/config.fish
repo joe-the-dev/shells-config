c9ff10a3ca5ffe8c76d6b65f99514844cb708c90
Joe Ta <joe.ta@pepperstone.com>
2025-09-15 10:34:21 +0700

    enhanced config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -11,7 +11,7 @@ end
 # Better Homebrew detection and setup
 if test -x /opt/homebrew/bin/brew
     eval (/opt/homebrew/bin/brew shellenv)
-elif test -x /usr/local/bin/brew
+else if test -x /usr/local/bin/brew
     eval (/usr/local/bin/brew shellenv)
 end
 
